b7d3a1fede783a630b73fb2a68980bf81ef9b091
pakin-pan <132637361+pakin-pan@users.noreply.github.com>
2023-05-17 09:14:04 +0000

    Assignment 4

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 int main(int argc, char **argv)
 {
@@ -7,8 +9,24 @@ int main(int argc, char **argv)
     a = new int[n];
   for (i=0;i<n;i++)
     {
-
-    
+      a[i] = atoi(*(argv+i+1));
+      cout<<"a["<<i<<"]= "<< a[i] << endl;
     }
-  
+  int *pa = a;
+  int *pb = a+n-1;
+  int temp;
+
+  for(i=0;i<n/2;i++)
+    {
+      temp = *pa;
+      *pa = *pb;
+      *pb = temp;
+      pa++;pb--;
+    }
+  pa-=(n/2);
+  for(i=0;i<n;i++)
+    {
+      cout << *(pa+i) << endl;
+    }
+  delete []a;  
 }
